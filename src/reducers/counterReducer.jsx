679821e5d9c0d7e955148fd863c8f79946719b9b
mirza <alexbrost7@gmail.com>
2022-03-15 22:34:40 +0300

    counter-redux

---
+++ src/reducers/counterReducer.jsx
@@ -0,0 +1,16 @@
+import { DECREASE_COUNT, INCREASE_COUNT } from "../actions/actionTypes";
+
+const initialState={
+    count:0
+}
+export default function counterREducer(state=initialState,action) {
+    switch (action.type) {
+        case INCREASE_COUNT:
+            return {count:state.count + 1}
+            case DECREASE_COUNT:
+                return{count:state.count - 1}
+        default:
+            return state;
+    }
+    
+}
